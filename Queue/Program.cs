27c952e49ab9d0f8ef8e1e97a0920ab7efa310d7
Randy Miller <Randy Miller@DESKTOP-7SQA22L>
2023-03-13 21:29:14 -0400

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Queue<string> grocery = new Queue<string>();
+
+            
+            grocery.Enqueue("Milk");
+            grocery.Enqueue("Juice");
+            grocery.Enqueue("Pineapple");
+            grocery.Enqueue("Bread");
+            grocery.Enqueue("Jam");
+
+            foreach (var g in grocery)
+            {
+                Console.WriteLine(g);
+            }
+
+            
+            string firstMem = grocery.Peek();
+            Console.WriteLine($"Would you like to remove {firstMem} from the beginning of the list (yes or no)?");
+            string answer = Console.ReadLine();
+            while (answer == "yes")
+            {
+                grocery.Dequeue(); 
+                firstMem = grocery.Peek();
+                Console.WriteLine($"Would you like to remove {firstMem} from the beginning of the queue (yes or no)?");
+                answer = Console.ReadLine();
+            }
+
+            if (grocery.Peek() == null)
+                Console.WriteLine("The queue is empty");
+            else
+                Console.WriteLine($"You have {grocery.Count} left in the list");
+
+        }
+    }
+}
